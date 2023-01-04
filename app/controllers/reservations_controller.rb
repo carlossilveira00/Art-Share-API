d3baf058ab3976528752f9afece2d72f317eb8a1
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-01-04 12:23:31 -0100

    Add method and route to Accept or Decline a Reservation.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,6 @@
 class ReservationsController < ApplicationController
   def index
-    @reservations = Reservation.includes(:item).where(["user_id = ? AND status = ?", params[:user_id], 'Pending'])
+    @reservations = Reservation.includes(:item).where(["user_id = ? AND status = ?", params[:user_id], 'Accepted'])
     @current_reservations = []
 
     @reservations.each do |reservation|
@@ -17,8 +17,34 @@ class ReservationsController < ApplicationController
     render json: ReservationBlueprint.render(@current_reservations)
   end
 
+  def handle_request
+    @reservation = Reservation.find(params[:id])
+
+    @reservation.update!(status: params[:status])
+
+    render json: @reservation
+  end
+
+  def renting_requests
+    @reservations = Reservation.includes(:item).where(items: { user_id: 3 }).where(status: 'Pending')
+    @requests = []
+
+    @reservations.each do |reservation|
+      @requests << {
+        id: reservation.id,
+        item_name: reservation.item.name,
+        starting_date: reservation.starting_date,
+        ending_date: reservation.ending_date,
+        status: reservation.status,
+        total_value: reservation.total_value
+      }
+    end
+
+    render json: ReservationBlueprint.render(@requests)
+  end
+
   def completed_reservations
-    @reservations = Reservation.includes(:item).where(["user_id = ? AND status = ?", params[:user_id], 'Completed'])
+    @reservations = Reservation.includes(:item).where(["user_id = ? AND status = ?", 3, 'Completed'])
     @completed_reservations = []
 
     @reservations.each do |reservation|
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   resources :items, only: [:index, :show, :create, :update, :destroy]
   resources :reservations, only: [:index, :create, :destroy]
   get '/completed_reservations', to: "reservations#completed_reservations"
+  patch 'handle_request', to: 'reservations#handle_request'
   get '/user_items', to: "items#user_items"
+  get '/pending_reservations', to: "reservations#renting_requests"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
